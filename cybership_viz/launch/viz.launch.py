8954e1725b625d3b40b8f653aa568d19e95d9307
Emir Cem Gezer <emircem.gezer@gmail.com>
2024-01-22 16:14:05 +0100

    feat: add visualization

---
+++ cybership_viz/launch/viz.launch.py
@@ -0,0 +1,26 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import *
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.launch_description_sources import *
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz',
+            output='screen',
+            arguments=[
+                "-d",
+                os.path.join(
+                    get_package_share_directory('cybership_viz'),
+                    'config',
+                    'cybership.rviz'
+                )
+            ]
+        ),
+    ])
