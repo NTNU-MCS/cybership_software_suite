c4f4f85549290f48d5aa3b0e142d5074626f3333
Emir Cem Gezer <emircem.gezer@gmail.com>
2024-01-24 16:23:39 +0100

    feat: add 3d model

--- cybership_viz/launch/viz.launch.py
+++ cybership_viz/launch/viz.launch.py
@@ -7,20 +7,33 @@ from launch_ros.actions import Node
 from launch.launch_description_sources import *
 
 def generate_launch_description():
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz',
+        output='screen',
+        arguments=[
+            "-d",
+            os.path.join(
+                get_package_share_directory('cybership_viz'),
+                'config',
+                'cybership.rviz'
+            )
+        ]
+    )
 
-    return LaunchDescription([
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz',
-            output='screen',
-            arguments=[
-                "-d",
-                os.path.join(
-                    get_package_share_directory('cybership_viz'),
-                    'config',
-                    'cybership.rviz'
+    # Description launch
+    launch_cybership_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('cybership_description'),
+                'launch',
+                'description.launch.py'
                 )
             ]
-        ),
+        )
+    )
+    return LaunchDescription([
+        rviz_node,
+        launch_cybership_description
     ])
