a57dc060a81455ba893261fa5a68a45938d76d70
Emir Cem Gezer <emircem.gezer@gmail.com>
2024-01-31 17:47:30 +0100

    feat: add teleop

---
+++ cybership_teleop/bin/cybership_enterprise1_tunnel.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rclpy
+import rclpy.node
+import geometry_msgs.msg
+import sensor_msgs.msg
+
+
+class CSEITeleop(rclpy.node.Node):
+
+    def __init__(self):
+        super().__init__('cse_teleop')
+
+        self.publisher = self.create_publisher(geometry_msgs.msg.Wrench, '/CSEI/thrusters/tunnel/command', 1)
+        self.subscriber = self.create_subscription(sensor_msgs.msg.Joy, '/joy', self.cb_joy, 10)
+
+    def cb_joy(self, msg):
+        if not (msg.buttons[9] or msg.buttons[10]):
+            return
+
+        wrench = geometry_msgs.msg.Wrench()
+        wrench.force.x = (msg.axes[5] - msg.axes[4]) / 2.0
+        self.publisher.publish(wrench)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    cse_teleop = CSEITeleop()
+
+    rclpy.spin(cse_teleop)
+
+    cse_teleop.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
