267e59177dea51d4642665bfea2c67f68c18d4ea
Emir Cem Gezer <emircem.gezer@gmail.com>
2025-03-04 09:25:44 +0100

    chore: add drillship templates

---
+++ cybership_bringup/launch/drillship.launch.py
@@ -0,0 +1,110 @@
+import launch_ros
+import launch
+import launch.actions
+import launch.substitutions
+import launch.launch_description_sources
+
+
+def include_launch_action_with_config(
+        vessel_model,
+        vessel_name,
+        launch_file,
+        param_file=""):
+
+    bringup_pkg_dir = launch_ros.substitutions.FindPackageShare(
+        'cybership_bringup')
+    config_pkg_dir = launch_ros.substitutions.FindPackageShare(
+        'cybership_config')
+
+    launch_arguments = [
+        ('vessel_name', vessel_name),
+        ('vessel_model', vessel_model)
+    ]
+
+    if len(param_file) != 0:
+        launch_arguments.append(
+            (
+                'param_file',
+                launch.substitutions.PathJoinSubstitution(
+                    launch.substitutions.PathJoinSubstitution(
+                        [config_pkg_dir, 'config', vessel_model, param_file]
+                    )
+                )
+            )
+        )
+
+    return launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            launch.substitutions.PathJoinSubstitution(
+                [bringup_pkg_dir, 'launch', 'include', launch_file]
+            )
+        ),
+        launch_arguments=launch_arguments
+    )
+
+
+def generate_launch_description():
+
+    vessel_name = 'drillship'
+
+    vessel_model = 'drillship'
+
+    ld = launch.LaunchDescription()
+
+    ld.add_action(
+        include_launch_action_with_config(
+            vessel_model, vessel_name,
+            'azimuth_controller.launch.py', 'azimuth_controller.yaml'
+        )
+    )
+
+    ld.add_action(
+        include_launch_action_with_config(
+            vessel_model, vessel_name,
+            'motion_capture_system_connector.launch.py', 'mocap_connector.yaml'
+        )
+    )
+
+    ld.add_action(
+        include_launch_action_with_config(
+            vessel_model, vessel_name,
+            'motion_capture_system_transformer.launch.py', 'mocap_transformer.yaml'
+        )
+    )
+
+    # ld.add_action(
+    #     include_launch_action_with_config(
+    #         vessel_model, vessel_name,
+    #         'robot_localization.launch.py', 'robot_localization.yaml'
+    #     )
+    # )
+
+    ld.add_action(
+        include_launch_action_with_config(
+            vessel_model, vessel_name,
+            'servo_driver.launch.py', 'servo_driver.yaml'
+        )
+    )
+
+    ld.add_action(
+        include_launch_action_with_config(
+            vessel_model, vessel_name,
+            'thruster_control.launch.py', 'thruster_control.yaml'
+        )
+    )
+
+    ld.add_action(
+        include_launch_action_with_config(
+            vessel_model, vessel_name,
+            'imu_bno055.launch.py', 'imu_bno055.yaml'
+        )
+    )
+
+    # ld.add_action(
+    #     include_launch_action_with_config(
+    #         vessel_model, vessel_name,
+    #         'urdf_description.launch.py', 'empty.yaml'
+    #     )
+    # )
+
+    return ld
--- dockerfile
+++ dockerfile
@@ -1,17 +1,15 @@
 # Set default values for build arguments
-ARG VESSEL_MODEL=voyager
 ARG ROS_DISTRO=humble
 ARG RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 
 # Use the specified ROS distribution as the base image
 FROM ros:$ROS_DISTRO
 
+SHELL [ "/bin/bash" ]
+
 # Set environment variables for ROS distribution and vessel model
 ARG ROS_DISTRO
 ENV ROS_DISTRO=${ROS_DISTRO}
-
-ARG VESSEL_MODEL
-ENV VESSEL_MODEL=${VESSEL_MODEL}
 
 ARG RMW_IMPLEMENTATION
 ENV RMW_IMPLEMENTATION=${RMW_IMPLEMENTATION}
@@ -40,9 +38,15 @@ RUN \
 RUN \
     rosdep install --from-paths src --ignore-src -r -y
 
+    # Install all RVM implementations
+RUN \
+    apt-get install -y \
+        ros-${ROS_DISTRO}-rmw-opensplice-cpp && \
+        ros-${ROS_DISTRO}-rmw-connext-cpp && \
+        ros-${ROS_DISTRO}-rmw-fastrtps-cpp
 
 # Build the ROS workspace
 RUN /bin/bash -c "source /opt/ros/${ROS_DISTRO}/setup.sh && colcon build"
 
 # Set the entry point to your launch file
-ENTRYPOINT ["/bin/bash", "-c", "source /ros/install/setup.sh && ros2 launch cybership_bringup ${VESSEL_MODEL}.launch.py"]
+ENTRYPOINT ["/bin/bash", "-c", "source /ros/install/setup.sh"]
